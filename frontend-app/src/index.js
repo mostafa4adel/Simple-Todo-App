4abe2598374c3f7bbbfc5441c244f53d3eee85e8
Mostafa Adel <es-mostafaadel2023@alexu.edu.eg>
2023-11-01 23:06:16 +0200

    Init Commit

---
+++ frontend-app/src/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function StartPage() {
+    return (
+        <View>
+            <Text>Welcome to My App</Text>
+            <Text>This is the start page of my app.</Text>
+        </View>
+    );
+}
+
+export default StartPage;
